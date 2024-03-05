ec7492153e2bd9287f9ce0686919d6957b5ab614
RenanFariaP <renanfariapereira@hotmail.com>
2024-03-05 08:57:54 -0300

    feat: error prevention

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,8 @@ let output_text = "";
 function criptografar() {
   input_text = input.value;
   input.value = "";
-  document.querySelector('.output_text').style.display = 'flex';
-  document.querySelector('.no_text').style.display = 'none';
+  document.querySelector(".output_text").style.display = "flex";
+  document.querySelector(".no_text").style.display = "none";
 
   for (let i in input_text) {
     console.log(input_text[i]);
@@ -24,7 +24,7 @@ function criptografar() {
       output_text += input_text[i];
     }
   }
-  document.querySelector('.result').value = output_text;
+  document.querySelector(".result").value = output_text;
   console.log(output_text);
   input_text = "";
   output_text = "";
@@ -33,84 +33,97 @@ function criptografar() {
 function descriptografar() {
   input_text = input.value;
   input.value = "";
-  document.querySelector('.output_text').style.display = 'flex';
-  document.querySelector('.no_text').style.display = 'none';
+  document.querySelector(".output_text").style.display = "flex";
+  document.querySelector(".no_text").style.display = "none";
   let i = 0;
-  console.log(input_text.length);
   while (i < input_text.length) {
     //Letra A
     if (input_text[i] === "a") {
       if (input_text[i + 1] === "i") {
         output_text += "a";
         i += 2;
+      } else {
+        output_text += "a";
+        i += 1;
       }
     }
     //Letra E
     else if (input_text[i] === "e") {
       if (input_text[i + 1] === "n") {
-        i += 1;
-        if (input_text[i + 1] === "t") {
-          i += 1;
-          if (input_text[i + 1] === "e") {
-            i += 1;
-            if (input_text[i + 1] === "r") {
-              i += 2;
+        if (input_text[i + 2] === "t") {
+          if (input_text[i + 3] === "e") {
+            if (input_text[i + 4] === "r") {
+              i += 5;
               output_text += "e";
             }
           }
         }
+      } else {
+        output_text += "e";
+        i += 1;
       }
     }
     //Letra I
     else if (input_text[i] === "i") {
       if (input_text[i + 1] === "m") {
-        i += 1;
-        if (input_text[i + 1] === "e") {
-          i += 1;
-          if (input_text[i + 1] === "s") {
-            i += 2;
+        if (input_text[i + 2] === "e") {
+          if (input_text[i + 3] === "s") {
+            i += 4;
             output_text += "i";
           }
         }
+      } else {
+        output_text += "i";
+        i += 1;
       }
     }
     //Letra O
     else if (input_text[i] === "o") {
       if (input_text[i + 1] === "b") {
-        i += 1;
-        if (input_text[i + 1] === "e") {
-          i += 1;
-          if (input_text[i + 1] === "r") {
-            i += 2;
+        if (input_text[i + 2] === "e") {
+          if (input_text[i + 3] === "r") {
+            i += 4;
             output_text += "o";
           }
         }
+      } else {
+        output_text += "o";
+        i += 1;
       }
     }
     //Letra U
     else if (input_text[i] === "u") {
       if (input_text[i + 1] === "f") {
-        i += 1;
-        if (input_text[i + 1] === "a") {
-          i += 1;
-          if (input_text[i + 1] === "t") {
-            i += 2;
+        if (input_text[i + 2] === "a") {
+          if (input_text[i + 3] === "t") {
+            i += 4;
             output_text += "u";
           }
         }
+      } else {
+        output_text += "e";
+        i += 1;
       }
     } else {
       output_text += input_text[i];
       i += 1;
     }
   }
-  document.querySelector('.result').value = output_text;
+  if (input_text === output_text) {
+    alert("Sua mensagem não está criptografada!");
+    document.querySelector(".output_text").style.display = "none";
+    document.querySelector(".no_text").style.display = "flex";
+  } else {
+    document.querySelector(".result").value = output_text;
+  }
   console.log(output_text);
   input_text = "";
   output_text = "";
 }
 
-async function copy(){
-  let copy_text = document.querySelector('.result').value;
+async function copy() {
+  let copy_text = document.querySelector(".result").value;
   await navigator.clipboard.writeText(copy_text);
+  document.querySelector(".output_text").style.display = "none";
+  document.querySelector(".no_text").style.display = "flex";
 }
